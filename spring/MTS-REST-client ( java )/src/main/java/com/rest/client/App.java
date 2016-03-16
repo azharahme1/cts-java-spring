19250f00c13a02de95e1b67d19af88e557633a77
nagabhushanam <nagabhushanamn@gmail.com>
2016-03-16 12:59:34 +0530

    day-5 commit

---
+++ spring/MTS-REST-client ( java )/src/main/java/com/rest/client/App.java
@@ -0,0 +1,30 @@
+package com.rest.client;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.client.RestTemplate;
+
+public class App {
+
+	public static void main(String[] args) {
+
+		String uri = "http://localhost:8080/mts/txr";
+
+		String fromAccNum = "1";
+		String toAccNum = "2";
+		double amount = 100.00;
+
+		// ----------------------------
+
+		TxrRequest request = new TxrRequest();
+		request.setFromAccNum(fromAccNum);
+		request.setToAccNum(toAccNum);
+		request.setAmount(amount);
+
+		RestTemplate restTemplate = new RestTemplate();
+		
+		ResponseEntity<TxrResponse> response = restTemplate.postForEntity(uri, request, TxrResponse.class);
+		System.out.println(response.getBody().getMessage());
+
+	}
+
+}
